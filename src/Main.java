db228232176394495ef2b1f3d32b92d38cd4989e
hjt <1161297340@qq.com>
2022-10-31 17:33:52 +0800

    20221031

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /*
+        A project to check duplications among some code.
+        @author hjt & hny
+        @email: 1161297340@qq.com
+     */
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Test");
+    }
+}
---
+++ src/StringCompare/CharacterCompare.java
@@ -0,0 +1,7 @@
+package StringCompare;
+    /*
+       check the code duplications by character;
+     */
+public class CharacterCompare {
+
+}
