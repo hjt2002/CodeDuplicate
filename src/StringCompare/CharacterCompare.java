914a44350b6e9978250019deceefb63f63e70b42
hjt <1161297340@qq.com>
2022-11-07 18:58:20 +0800

    first commit in char branch

--- src/StringCompare/CharacterCompare.java
+++ src/StringCompare/CharacterCompare.java
@@ -1,7 +1,38 @@
 package StringCompare;
-    /*
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
        check the code duplications by character;
      */
 public class CharacterCompare {
+    List<String> keyword_list = new ArrayList<>();
+    String[] keyword = {"auto", "break", "case", "char", "const", "continue", "default",
+            "do", "double", "else", "enum", "extern", "float", "for", "goto", "if", "int",
+            "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch",
+            "typedef", "union", "unsigned", "void", "volatile", "while"};
+    private File file1 = null;
+    private File file2 = null;
+    private FileInputStream input = null;
+    private FileOutputStream output = null;
+
+    public CharacterCompare(String filepath1, String filepath2) throws FileNotFoundException {
+        file1 = new File(filepath1);
+        file2 = new File(filepath2);
+
+        if (file1 != null && file2 != null) {
+            input = new FileInputStream(file1);
+            output = new FileOutputStream(file2);
+        }
+        //add the keyword into the list
+        keyword_list.addAll(Arrays.asList(keyword));
+    }
+
 
 }
